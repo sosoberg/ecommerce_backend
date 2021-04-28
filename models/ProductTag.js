1f6d49072edfaea74b45e25f8cfdfc4d710e424b
Sam Oberg <samueloberg@hotmail.com>
2021-04-27 18:29:46 -0700

    added columns for the MYSQL tables

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,28 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // defined columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: Product.id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: Tag.id
+    }
   },
   {
     sequelize,
