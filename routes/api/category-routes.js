6c567c94340a53349f740e5cf5a9f8d20fc12e25
Sam Oberg <samueloberg@hotmail.com>
2021-05-02 13:17:14 -0700

    finishing update commands for api

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,6 +54,20 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 });
 
